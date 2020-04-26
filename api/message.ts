e890f8ca0bad0003d51d9eedb55f7ecc4c891f52
yuler <joyful4yu@gmail.com>
2020-04-26 23:08:37 +0800

    Add api/message simple handler

---
+++ api/message.ts
@@ -0,0 +1,33 @@
+import { NowRequest, NowResponse } from '@now/node'
+import got from 'got'
+
+const { TOKEN: token } = process.env
+
+enum MessageType {
+  TEXT = 0,
+  IMAGE = 1,
+  URL_LINK = 2,
+  FILE = 3,
+}
+
+export default (req: NowRequest, res: NowResponse) => {
+  const { chatId, payload } = req.body.data
+  ;(() => {
+    try {
+      got.post('https://ex-api.botorange.com/message/send', {
+        json: {
+          chatId,
+          token,
+          messageType: MessageType.TEXT,
+          payload: {
+            text: payload.text,
+            mention: payload.mention
+          }
+        }
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  })()
+  res.end()
+}
--- index.js
+++
@@ -1,7 +0,0 @@
-const { json, send } = require('micro')
-
-module.exports = (res, req) => {
-  const data = await json(req)
-  console.log(data)
-  return 'xxx'
-}
